7f14d774127f63ef1733b5bb6854f8e4d1089b62
Wahyu IT ODT <47714314+wahyusa83@users.noreply.github.com>
2024-01-15 10:14:48 +0800

    Update util.sh

--- BUILD/util.sh
+++ BUILD/util.sh
@@ -1,5 +1,5 @@
 # MariaDB SQL server.
-# Copyright (C) 2010 Kristian Nielsen and Monty Program AB
+# Copyright (C) 2024 Nirwana Computer and Monty Program AB
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
