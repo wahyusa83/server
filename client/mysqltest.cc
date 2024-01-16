1b8c0873dfa6812b8ec934325d2454a09d4da5a3
Wahyu IT ODT <47714314+wahyusa83@users.noreply.github.com>
2024-01-17 05:53:31 +0800

    Update mysqltest.cc

--- client/mysqltest.cc
+++ client/mysqltest.cc
@@ -1,5 +1,5 @@
-/* Copyright (c) 2000, 2013, Oracle and/or its affiliates.
-   Copyright (c) 2009, 2022, MariaDB
+/* Copyright (c) 2022, 2023, Oracle and/or its affiliates.
+   Copyright (c) 2022, 2024, NirwanaComp
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
