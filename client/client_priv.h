56ecad12c80b0d7ff5690bfd3b6264355ff75116
Wahyu IT ODT <47714314+wahyusa83@users.noreply.github.com>
2024-01-17 05:42:25 +0800

    Update client_priv.h

--- client/client_priv.h
+++ client/client_priv.h
@@ -1,6 +1,6 @@
 /*
-   Copyright (c) 2001, 2012, Oracle and/or its affiliates.
-   Copyright (c) 2009, 2022, MariaDB
+   Copyright (c) 2022, 2023, Oracle and/or its affiliates.
+   Copyright (c) 2022, 2024, NirwanaComp
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
