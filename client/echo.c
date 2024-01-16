f43f922d78152a813c598a2925a3943a9836a0e6
Wahyu IT ODT <47714314+wahyusa83@users.noreply.github.com>
2024-01-17 05:43:23 +0800

    Update echo.c

--- client/echo.c
+++ client/echo.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2000, 2007 MySQL AB
+/* Copyright (c) 2022, 2024 MySQL AB
    Use is subject to license terms
 
    This program is free software; you can redistribute it and/or modify
